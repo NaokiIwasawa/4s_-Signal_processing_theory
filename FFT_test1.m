4e30c21c6a42141087c399a18a7dca3d0a43d8c8
NaokiIwasawa <s20C1016CT@s.chibakoudai.jp>
2022-01-09 03:00:43 +0900

    change x-axis

--- FFT_test1.m
+++ FFT_test1.m
@@ -3,6 +3,7 @@ clear;
 voice = 'kakikukeko_E.wav';
 [x,fs] = audioread(voice);
 pspectrum(x, fs,'spectrogram'); 
+x = x(2e4:5e4);
 
 plot(x)
 xlabel('Sample Number')
---
+++ FFT_test1_a.m
@@ -0,0 +1,10 @@
+clear;
+%voice = 'aiueo_E.wav';
+voice = 'kakikukeko_E.wav';
+[x,fs] = audioread(voice);
+pspectrum(x, fs,'spectrogram'); 
+%x = x(2e4:5e4);
+
+plot(x)
+xlabel('Sample Number')
+ylabel('Amplitude')
