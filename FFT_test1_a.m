cf630e6d9d1cbd0d8dfb32afb79c1cb34cc21e70
NaokiIwasawa <s20C1016CT@s.chibakoudai.jp>
2022-01-09 04:15:47 +0900

    add test1 picture and fig

--- FFT_test1_a.m
+++ FFT_test1_a.m
@@ -3,7 +3,12 @@ clear;
 voice = 'kakikukeko_E.wav';
 [x,fs] = audioread(voice);
 pspectrum(x, fs,'spectrogram'); 
+
 %x = x(2e4:5e4);
+%x = x(5.2e4:7.5e4);
+%x = x(7.8e4:10.2e4);
+%x = x(10.5e4:13e4);
+x = x(13.3e4:15.2e4);
 
 plot(x)
 xlabel('Sample Number')
